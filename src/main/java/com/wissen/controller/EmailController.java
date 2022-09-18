66b05207bfac8330ef592831baf72ea6947811eb
vishaltomar228 <vishaltomar228@gmail.com>
2022-09-18 15:35:08 +0530

    Adding background image in email and putting name on it

--- src/main/java/com/wissen/controller/EmailController.java
+++ src/main/java/com/wissen/controller/EmailController.java
@@ -5,6 +5,7 @@ import com.wissen.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
@@ -19,15 +20,19 @@ public class EmailController {
     private EmailService emailService;
 
     @GetMapping
-    public void sendEmail() throws MessagingException {
+    public void sendEmail(@RequestParam("name") final String name) throws MessagingException {
         Email email = new Email();
+
+        /* When we will add scheduler will remove all this and add details through properties file and
+        database.
+         */
         email.setTo("vishaltomar228@gmail.com");
-        email.setFrom("employeemanagement321@gmail.com");
+        email.setFrom("vishal.tomar@wissen.com");
+        email.setCc("vishal.tomar@wissen.com");
         email.setSubject("Welcome Email");
         email.setTemplate("EMAIL_TEMPLATE.html");
-        email.getProperties().put("name", "vishal");
-        email.getProperties().put("subscriptionDate", LocalDate.now().toString());
-        email.getProperties().put("technologies", Arrays.asList("Python", "Go", "C#"));
+        email.getProperties().put("name", name);
+
         email.setProperties(email.getProperties());
         emailService.sendHtmlMessage(email);
     }
