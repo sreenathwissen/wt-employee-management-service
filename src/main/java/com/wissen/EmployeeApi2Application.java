ddbb28bc544651c0c90e164b6d6cea881a9eba13
anushkasaxena <anushka.saxena.68@gmail.com>
2023-01-08 20:28:41 +0530

    Dockerize

--- src/main/java/com/wissen/EmployeeApi2Application.java
+++ src/main/java/com/wissen/EmployeeApi2Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -16,6 +18,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableScheduling
 public class EmployeeApi2Application {
+//
+//	@RequestMapping("/")
+//	public String home(){
+//		return "Docker home controller!";
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApi2Application.class, args);
---
+++ src/main/java/com/wissen/HomeController.java
@@ -0,0 +1,13 @@
+package com.wissen;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Docker home controller!";
+    }
+}
