9da78f597dc2f835f4fbfafd5f4b1d039b35b9ad
vishaltomar228 <vishaltomar228@gmail.com>
2023-03-02 23:24:09 +0530

    Swagger fix

--- src/main/java/com/wissen/config/WebMvcConfiguration.java
+++ src/main/java/com/wissen/config/WebMvcConfiguration.java
@@ -2,10 +2,7 @@ package com.wissen.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * Web configuration class.
@@ -14,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableWebMvc
-public class WebMvcConfiguration {
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -25,6 +22,18 @@ public class WebMvcConfiguration {
 						"GET", "POST");
 			}
 		};
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry
+				.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry
+				.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
